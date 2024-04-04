36db18693855065aedd501bc57af89da21e1b925
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2024-04-04 22:20:16 +0100

    update nav bar

--- src/components/app/Navbar.tsx
+++ src/components/app/Navbar.tsx
@@ -12,7 +12,7 @@ const NavigationBar = ({search, setSearch}:NavProp) => {
 
   return (
     <nav className={`sticky top-0 z-50 bg-teal-700 flex justify-between items-center h-16 px-4`}>
-      <div className="md-text-md lg:text-lg text-white font-bold">West African Countries</div>
+      <div className="xs:text-xs sm:text-sm md:text-md lg:text-lg text-white font-bold">West African Countries</div>
       <div className="flex items-center">
         <input
           type="text"
@@ -20,6 +20,7 @@ const NavigationBar = ({search, setSearch}:NavProp) => {
           value={search}
           onChange={(e)=>setSearch(e.target.value)}
           className={`bg-teal-800 text-white rounded-md px-4 py-2
+          xs:text-xs sm:text-sm md:text-md lg:text-lg
            focus:outline-none focus:ring-2 focus:ring-teal-300 sm:w-full`}
         />
       </div>
